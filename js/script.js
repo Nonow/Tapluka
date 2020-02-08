97b21a93191c16e17bf8dd9977d7a76c2544bf9b
Steave <56349889+SteaveDev@users.noreply.github.com>
2020-02-08 10:30:50 +0100

    Ajout des fichiers externes

---
+++ js/script.js
@@ -0,0 +1,15 @@
+(function($) {
+
+  $("a.btn-appoint").on('click', function(event) {
+    var hash = this.hash;
+    if (hash) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function() {
+        window.location.hash = hash;
+      });
+    }
+  });
+
+})(jQuery);
