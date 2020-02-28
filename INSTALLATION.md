5e976cff27e923cca058de936f1bd556846fb87d
Adrien Cavalieri <adr.cavalieri@gmail.com>
2020-02-28 13:18:41 +0100

    Ajout d'un tutoriel, complétion du model et installation du module mysql

---
+++ INSTALLATION.md
@@ -0,0 +1,10 @@
+# Installation
+
+1. Installer le paquet "nodejs" sous votre distribution GNU/Linux (Debian, Arch, Manjaro, etc.).
+2. Cloner ce dépôt Github sur votre machine.
+
+# Lancement
+
+1. Rendez-vous sur cette branche sur votre machine grâce à la commande `git checkout testNodeJS`.
+2. Lancer le serveur grâce à la commande `npm start`.
+3. Se rendre sur http://localhost:3000
--- models/model.js
+++ models/model.js
@@ -7,6 +7,13 @@ class Model {
             cb(rows)
         })
     }
+
+    static recette(cb) {
+        connection.query("select * from recettes natural join imageRecette natural join recette_ingredient natural join ingrédients natural join recette_typeRecette natural join typeRecette;", (err, rows) => {
+            if (err) throw err
+            console.log(rows)
+        })
+    }
 }
 
 module.exports = Model
