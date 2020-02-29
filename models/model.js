ad905a5ee961dfff9d87d1ab4538fb1b1c31ad00
Antony Fourchault <antonyfourchault@gmail.com>
2020-02-29 17:46:30 +0100

    famille add+remove

--- models/model.js
+++ models/model.js
@@ -12,30 +12,72 @@ let connection = require('../bin/bdd');
 }*/
 
 class Model {
-    static test(cb){
-        connection.query("select * from utilisateurs;",(err,rows)=> {
+    static test(cb) {
+        connection.query("select * from utilisateurs;", (err, rows) => {
             if (err) throw err
             cb(rows)
         })
     }
-/*
-(select sA.nom from familles f
-    join famille_spec fs on f.id = fs.id_famille
-    join specificationsAlimentaires sA on fs.id_spec = sA.id
-where f.id_utilisateur = 4 )
-union
-(select sA.nom from utilisateur_spec us
-    join specificationsAlimentaires sA on us.id_spec = sA.id
-where us.id_utilisateur=4) ;
- */
 
-    static famille(idUser,cb){
-        connection.query('select * from familles where id_utilisateur = ?',[idUser], (err, rows)=>{
+    /*
+    (select sA.nom from familles f
+        join famille_spec fs on f.id = fs.id_famille
+        join specificationsAlimentaires sA on fs.id_spec = sA.id
+    where f.id_utilisateur = 4 )
+    union
+    (select sA.nom from utilisateur_spec us
+        join specificationsAlimentaires sA on us.id_spec = sA.id
+    where us.id_utilisateur=4) ;
+     */
+
+    static famille(idUser, cb) {
+        connection.query('select * from familles where id_utilisateur = ?', [idUser], (err, rows) => {
             if (err) throw err
-            connection.query('select * from famille_spec fs join specificationsAlimentaires sA on fs.id_spec = sA.id',(err, rows2)=>{
+            connection.query('select * from famille_spec fs join specificationsAlimentaires sA on fs.id_spec = sA.id', (err, rows2) => {
                 if (err) throw err
-                cb(rows,rows2)
+                connection.query("select * from specificationsAlimentaires", (err, rows3) => {
+
+                    cb(rows, rows2, rows3)
+                })
+
             })
+        })
+    }
+
+    static addFamille(idUser, data, cb) {
+        var newId = Math.floor(Math.random() * 1000000);
+        connection.query("insert into familles values (?,?,?,?);", [newId, idUser, data.nom, data.type], (err, rows) => {
+            if (err) throw err
+            var sql = "insert into famille_spec values"
+            for (var i = 0; i < data.specAlim.length; i++) {
+                if (i == 0) {
+                    sql += "(" + newId + "," + data.specAlim[i] + ")"
+                } else {
+                    sql += ",(" + newId + "," + data.specAlim[i] + ")"
+                }
+            }
+            connection.query(sql, (err, rows) => {
+                if (err) throw err
+                cb("OK")
+            })
+
+        })
+    }
+
+    static removeFamille(idUser, idFamille, cb) {
+        connection.query("select count(*) as existUser from familles where id_utilisateur = ? and id=?", [idUser, idFamille], (err, rows) => {
+            if (err) throw err
+            if (rows.length > 0) {
+                connection.query("delete from famille_spec where id_famille = ?", [idFamille], (err, rows) => {
+                    if (err) throw err
+                    connection.query("delete from familles where id=?", [idFamille], (err, rows) => {
+                        if (err) throw err
+                        cb("OK")
+                    })
+                })
+            } else {
+                cb("KO")
+            }
         })
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,65 @@
 var express = require('express');
 var router = express.Router();
 var model = require('../models/model.js')
+var currentUser = 3
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  model.test(function (profils) {
-    res.render('index', { title: 'index', profils : profils });
-  })
+router.get('/', function (req, res, next) {
+    model.test(function (profils) {
+        res.render('index', {title: 'index', profils: profils});
+    })
 
 });
 
-router.get('/connexion', function(req, res, next) {
-  res.render('connexion', { title: 'connexion' });
+router.get('/connexion', function (req, res, next) {
+    res.render('connexion', {title: 'connexion'});
 });
 
-router.get('/inscription', function(req, res, next) {
-  res.render('inscription', { title: 'inscription' });
+router.get('/inscription', function (req, res, next) {
+    res.render('inscription', {title: 'inscription'});
 });
 
-router.get('/listecourse', function(req, res, next) {
-  res.render('listecourse', { title: 'listecourse' });
+router.get('/listecourse', function (req, res, next) {
+    res.render('listecourse', {title: 'listecourse'});
 });
 
-router.get('/regles', function(req, res, next) {
-  res.render('regles', { title: 'regles' });
+router.get('/regles', function (req, res, next) {
+    res.render('regles', {title: 'regles'});
 });
 
-router.get('/recette', function(req, res, next) {
-  res.render('recette', { title: 'recette' });
+router.get('/recette', function (req, res, next) {
+    res.render('recette', {title: 'recette'});
+});
+
+router.get('/famille', function (req, res, next) {
+    model.famille(currentUser, function (famille, specAlim, allSA) {
+        res.render('famille', {title: 'famille', famille: famille, specAlim: specAlim, allSA: allSA});
+    })
+});
+
+router.post('/famille', function (req, res, next) {
+    console.log(req.body)
+    if (req.body.form == "add") {
+        model.addFamille(currentUser, req.body, function (status) {
+            res.redirect('/famille')
+        })
+    } else if (req.body.form == "remove") {
+        model.removeFamille(currentUser, req.body.id, function (status) {
+            res.redirect('/famille')
+        })
+    } else {
+        res.redirect('/famille')
+    }
+
+
+});
+
+router.get('/modifier_famille', function (req, res, next) {
+    res.redirect('/famille')
+});
+
+router.get('/modifier_famille/:id', function (req, res, next) {
+    res.render('modifier_famille', {title: 'modifier_famille'});
 });
 
 module.exports = router;
