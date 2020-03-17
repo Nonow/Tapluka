69d74260547e6cd8488c5e3ae91e98ab0bc48aa3
mathieu <mathieujugi@gmail.com>
2020-03-17 19:34:42 +0100

    bdd/paquets/fam mathieu

--- bin/bdd.js
+++ bin/bdd.js
@@ -1,18 +1,18 @@
 let mysql = require('mysql');
-/*let connection = mysql.createConnection({
+let connection = mysql.createConnection({
     host: 'remotemysql.com',
     user: 'Um177LwAqA',
     password: 'j5GGwOJHsi',
     database: 'Um177LwAqA',
     insecureAuth : true,
-});*/
+});/*
 let connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'm1-gestionprojet',
     insecureAuth : true,
-});
+});*/
 
 connection.connect(function(err) {
     if (err) throw err;
